f6538cb4421aa690c97b131558873d2150160e32
Andrey Novikov <novikov@gmail.com>
2018-12-11 18:37:19 +0300

    First commit

---
+++ apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class DateRangeFilterAppConfig(AppConfig):
+    name = 'daterangefilter'
+    verbose_name = _('Date Range Filter')
---
+++ filters.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+from django.template.defaultfilters import slugify
+from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+
+
+class DateRangeFilter(admin.FieldListFilter):
+    def __init__(self, field, request, params, model, model_admin, field_path):
+        self.lookup_kwarg_gte = '{}__gte'.format(field_path)
+        self.lookup_kwarg_lte = '{}__lte'.format(field_path)
+        self.lookup_gte = params.get(self.lookup_kwarg_gte)
+        self.lookup_lte = params.get(self.lookup_kwarg_lte)
+        # todo: check if this is required in default admin
+        if self.lookup_gte == '':
+            params.pop(self.lookup_kwarg_gte)
+        if self.lookup_lte == '':
+            params.pop(self.lookup_kwarg_lte)
+        if self.lookup_gte and self.lookup_lte:
+            self.lookup_val = '{} - {}'.format(self.lookup_gte.replace('-','.'), self.lookup_lte.replace('-','.'))
+        else:
+            self.lookup_val = ''
+        super(DateRangeFilter, self).__init__(field, request, params, model, model_admin, field_path)
+
+    def get_template(self):
+        if 'suit' in settings.INSTALLED_APPS:
+            return 'daterangefilter/suit_daterangefilter.html'
+        else:
+            return 'daterangefilter/daterangefilter.html'
+
+    def choices(self, changelist):
+        yield {
+            'value': self.lookup_val,
+            'query_string': changelist.get_query_string(remove=self._get_expected_fields())
+        }
+
+    def expected_parameters(self):
+        return self._get_expected_fields()
+
+    def _get_expected_fields(self):
+        return [self.lookup_kwarg_gte, self.lookup_kwarg_lte]
+
+    template = property(get_template)

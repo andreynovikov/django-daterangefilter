6998168feed3b3938fe11533ce041365b0ae3db8
Yuri Pimenov <pimenoff@gmail.com>
2022-12-14 23:08:56 +0200

    Parse with datetime.fromisoformat if strptime '%Y-%m-%d' fails

--- daterangefilter/filters.py
+++ daterangefilter/filters.py
@@ -27,19 +27,25 @@ class DateRangeFilter(admin.FieldListFilter):
                 field = model._meta.get_field(related_model).related_model._meta.get_field(field)
             else:
                 field = model._meta.get_field(field_path)
-                
+
             if isinstance(field, models.DateTimeField):
                 try:
                     gte_date = datetime.datetime.strptime(self.lookup_gte, '%Y-%m-%d')
                     lte_date = datetime.datetime.strptime(self.lookup_lte, '%Y-%m-%d')
                     lte_date = lte_date + datetime.timedelta(seconds=3600*24-1)
-                    if settings.USE_TZ:
-                        gte_date = timezone.make_aware(gte_date, timezone.get_current_timezone())
-                        lte_date = timezone.make_aware(lte_date, timezone.get_current_timezone())
-                    params[self.lookup_kwarg_gte] = gte_date.strftime('%Y-%m-%d %H:%M:%S%z')
-                    params[self.lookup_kwarg_lte] = lte_date.strftime('%Y-%m-%d %H:%M:%S%z')
                 except ValueError:
-                    messages.add_message(request, messages.ERROR, _("Invalid date for '%(field_name)s' field range filter") % {'field_name': field.verbose_name})
+                    try:
+                        gte_date = datetime.datetime.fromisoformat(self.lookup_gte)
+                        lte_date = datetime.datetime.fromisoformat(self.lookup_lte)
+                    except ValueError:
+                        messages.add_message(request, messages.ERROR, _("Invalid date for '%(field_name)s' field range filter") % {'field_name': field.verbose_name})
+
+                if settings.USE_TZ:
+                    gte_date = timezone.make_aware(gte_date, timezone.get_current_timezone())
+                    lte_date = timezone.make_aware(lte_date, timezone.get_current_timezone())
+
+                params[self.lookup_kwarg_gte] = gte_date.strftime('%Y-%m-%d %H:%M:%S%z')
+                params[self.lookup_kwarg_lte] = lte_date.strftime('%Y-%m-%d %H:%M:%S%z')
         else:
             self.lookup_val = ''
         super(DateRangeFilter, self).__init__(field, request, params, model, model_admin, field_path)

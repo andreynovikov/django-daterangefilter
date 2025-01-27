20904d9f9dd775a3d35eed936b514b731fdebb9a
Andrey Novikov <novikov@gmail.com>
2025-01-27 17:09:46 +0300

    Fix tests

--- runtests.py
+++ runtests.py
@@ -15,14 +15,34 @@ settings.configure(
         'django.contrib.sites',
         'django.contrib.admin',
         'django.contrib.sessions',
+        'django.contrib.messages',
         'daterangefilter',
+    ),
+    MIDDLEWARE=(
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+    ),
+    TEMPLATES=(
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'OPTIONS': {
+                'context_processors': (
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                )
+            },
+        },
     ),
     DATABASES={
         'default': {'ENGINE': 'django.db.backends.sqlite3'}
     },
+    DEFAULT_AUTO_FIELD='django.db.models.BigAutoField',
     TEST_RUNNER='django.test.runner.DiscoverRunner',
     USE_TZ=True,
     TIME_ZONE='UTC',
+    SECRET_KEY='*',
 )
 
 django.setup()

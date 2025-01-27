3374f952fe9dc57a1ac8b4f8a5a72f831f687b4a
Andrey Novikov <novikov@gmail.com>
2025-01-27 19:51:39 +0300

    Release 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='django-daterangefilter',
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
     author='Andrey Novikov',
     author_email='novikov@gmail.com',
